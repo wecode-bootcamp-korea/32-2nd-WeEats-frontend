8b1b9fb59345329f3aa07f2082397b41ca15a7a4
cks612 <kyusung612@gmail.com>
2022-05-11 20:28:05 +0900

    [32기 최규성] 카카오 지도 api 구현 및 공공데이터 활용 conflict 해결

---
+++ src/components/KaKaoMap/KaKaoMap.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const { kakao } = window;
+
+// To-Do
+// 소셜로그인 머지되면
+// API키 .env파일로 옮기기
+const api = '6279564769636b73313231664a507774';
+
+function KaKaoMap() {
+  const [shopInfo, setShopInfo] = useState([]);
+  useEffect(() => {
+    fetch(
+      `http://openapi.seoul.go.kr:8088/${api}/json/CrtfcUpsoInfo/1/1000/
+      `,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      }
+    )
+      .then(res => res.json())
+      .then(res => {
+        const seoulFoodStore = res.CrtfcUpsoInfo.row.map(result => {
+          return {
+            title: result.UPSO_NM,
+            latlng: new kakao.maps.LatLng(
+              parseFloat(result.Y_DNTS),
+              parseFloat(result.X_CNTS)
+            ),
+            address: result.RDN_CODE_NM,
+            category: result.BIZCND_CODE_NM,
+            tellNo: result.TEL_NO,
+          };
+        });
+
+        setShopInfo(seoulFoodStore);
+      });
+  }, []);
+
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(37.504056, 127.047229),
+      level: 8,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+    const imageSrc =
+      'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+
+    const imageSize = new kakao.maps.Size(24, 35);
+
+    shopInfo.length &&
+      shopInfo.forEach(shop => {
+        const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+        const marker = new kakao.maps.Marker({
+          map,
+          position: shop.latlng,
+          title: shop.title,
+          image: markerImage,
+        });
+
+        const content = `
+        <div className="overLayWrapper" style='position: absolute;left: 0;bottom: 40px;width: 288px;height: 132px;margin-left: -144px;text-align: left;font-size: 12px;line-height: 1.5; '>
+        <div className="overLayContainer" style='position: relative;width:306px;height: 120px;border-radius: 20px;border-bottom: 2px solid #ccc;border-right: 1px solid #ccc;background: #fff; box-shadow: 2px 2px 2px 1px gray;'>
+          <div className="shopName" style='position: absolute;padding:4px 0 0 5px;width:306px;height: 30px;background: #5CC8F0;border-bottom: 1px solid #ddd;font-size: 15px;font-weight: bold; border-radius:10px 10px 0 0; color: #fff; '>
+            ${shop.title}
+          </div>
+          <div className="shopContent">
+            <div className="shopImg" style='position: absolute;top: 35px;left: 5px;width: 73px;height: 71px;border: 1px solid #ddd;color: #888;overflow: hidden; border-style:none;'>
+              <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbPxnqX%2FbtrCakOPeQx%2FX2I6QkA9Y13DXodMhCW2k1%2Fimg.png" alt="" style="width:100%; height:100%; "/>
+            </div>
+            <div className="shopAddress" style='display:flex; align-items:flex-start;  flex-direction:column; width:200px; margin: 13px 0 0 85px;padding-top:40px; font-size: 11px;font-weight:900;color: #888;margin-top: -2px;'>
+            <p style='width:100%; white-space:normal; word-break:keep-all;'>주소 : <span style='font-weight:500;'>${shop.address}<span></p>
+            <p>종류 : <span style='font-weight:500;'>${shop.category}<span></p>
+            <p>전화번호 : <span style='font-weight:500;'>${shop.tellNo}<span></p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+        `;
+
+        const overlay = new kakao.maps.CustomOverlay({
+          content,
+          position: marker.getPosition(),
+        });
+
+        kakao.maps.event.addListener(marker, 'mouseover', function () {
+          overlay.setMap(map);
+        });
+
+        kakao.maps.event.addListener(marker, 'mouseout', function () {
+          overlay.setMap(null);
+        });
+      });
+  }, [shopInfo]);
+
+  return <KakaoMapDiv />;
+}
+export default KaKaoMap;
+
+const KakaoMapDiv = styled.div.attrs({
+  id: 'map',
+})`
+  width: 800px;
+  height: 400px;
+  margin: 0 auto;
+  border: 1px solid black;
+  border-radius: 30px;
+`;
