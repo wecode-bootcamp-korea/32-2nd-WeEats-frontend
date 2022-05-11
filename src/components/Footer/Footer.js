b69c30bf40cdef0872081db32eb7230d04fe9216
cks612 <kyusung612@gmail.com>
2022-05-11 13:27:00 +0900

    [32기 최규성] Footer 레이아웃 리뷰반영 수정완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,40 @@
+import * as footer from './Footer.style';
+
+const BottomList = [
+  { id: 1, info: '브랜드 스토리' },
+  { id: 2, info: '회사소개' },
+  { id: 3, info: '채용정보' },
+  { id: 4, info: '이용약관' },
+  { id: 5, info: '개인정보처리방침' },
+  { id: 6, info: '공지사항' },
+  { id: 7, info: '고객센터' },
+  { id: 8, info: '고객의 소리' },
+  { id: 9, info: '전문가 등록' },
+  { id: 10, info: '사업자 구매회원' },
+  { id: 11, info: '제휴/광고 문의' },
+  { id: 12, info: '입점신청 문의' },
+];
+
 const Footer = () => {
-  return <footer>푸터</footer>;
+  return (
+    <footer.FooterContainer>
+      <footer.FooterTop>
+        <footer.FooterTopInfo>
+          <span>고객센터</span>
+          <strong>1600-0123</strong>
+          <span>평일 09:00 ~ 18:00 (주말 & 공휴일 제외)</span>
+        </footer.FooterTopInfo>
+      </footer.FooterTop>
+      <footer.InfoList>
+        {BottomList.map(list => {
+          return <li key={list.id}>{list.info}</li>;
+        })}
+      </footer.InfoList>
+      <footer.CopyRight>
+        Copyright 2022. WeEats, Co, Ltd, All rights reserved
+      </footer.CopyRight>
+    </footer.FooterContainer>
+  );
 };
 
 export default Footer;
---
+++ src/components/Footer/Footer.style.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+export const FooterContainer = styled.div`
+  display: block;
+  max-width: 1256px;
+  margin: 0 auto;
+  padding: 30px 40px 40px;
+`;
+
+export const FooterTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FooterTopInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin: 0 0 25px;
+  line-height: 22px;
+  color: ${({ theme }) => theme.fontcolor};
+
+  > span {
+    font-size: 12px;
+  }
+
+  strong {
+    font-size: 28px;
+    font-weight: 700;
+  }
+`;
+
+export const InfoList = styled.ul`
+  margin: 0 -5px -10px;
+  list-style: none;
+
+  li {
+    display: inline;
+    padding-right: 10px;
+    font-size: 12px;
+  }
+`;
+
+export const CopyRight = styled.p`
+  margin: 10px -5px 0;
+  font-size: 10px;
+  line-height: 30px;
+`;
